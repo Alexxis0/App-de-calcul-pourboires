79e71e7d01aac14caee3944564b47e7aaba7a50e
Alexis Morantin <alexismorantin44390@gmail.com>
2022-12-03 15:23:20 +0100

    focus btn css js

---
+++ index.js
@@ -0,0 +1,27 @@
+let bill = document.querySelector(".dollar");
+let peopleNumber = document.querySelector(".people_number");
+let pourcent = document.querySelector(".pourcent");
+let pourcent5 = document.querySelector("input[value='5%']");
+let pourcent10 = document.querySelector("input[value='10%']");
+let pourcent15 = document.querySelector("input[value='15%']");
+let pourcent25 = document.querySelector("input[value='25%']");
+let pourcent50 = document.querySelector("input[value='50%']");
+let pourcentNumber = [pourcent5, pourcent10, pourcent15, pourcent25, pourcent50];
+
+bill.addEventListener("change", () => {
+    let billValue = bill.value;
+    console.log(billValue);
+});
+peopleNumber.addEventListener("change", () => {
+    let peopleValue = peopleNumber.value;
+    console.log(peopleValue);
+});
+
+function stylePourcent() {
+    pourcentNumber.forEach((element) => {
+        element.addEventListener("click", () => {
+            element.classList.toggle("pourcentStyle");
+        });
+    });
+}
+stylePourcent();
